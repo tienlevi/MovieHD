78eb3039b11c7ced093bbcbfba9248bb83d9b62e
TiennLevii <nguyentrachtien2401@gmail.com>
2024-02-26 23:10:41 +0700

    fix UI

--- client/src/components/Movies/Movies.tsx
+++ client/src/components/Movies/Movies.tsx
@@ -7,12 +7,12 @@ function Movies({ items }: { items: MovieTopRate[] }) {
   return (
     <>
       <Section className="top-rate">
-        <h1 className="">Top rate</h1>
+        <h1 className="top-rate-title">Top rate</h1>
         <div className="top-rate-movie">
           {items.map((item) => (
             <div className="top-rate-movie-children" key={item?.id}>
-              <img width={300} src={ImageMovie(item.poster_path)} alt="" />
-              <h2 key={item?.id} className="list-movie-text">
+              <img src={ImageMovie(item.poster_path)} alt="" />
+              <h2 key={item?.id} className="top-rate-movie-children">
                 {item.title}
               </h2>
             </div>
