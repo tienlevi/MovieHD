a89c04464119bf6f077f848876a73a85310fab48
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-03 22:29:26 +0700

    Update UI Footer and change max-width section

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,109 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Section from "../Section/Section";
+import { imageSrc } from "../../constants";
+import "./style.scss";
 
 function Footer() {
-  return <div>Footer</div>;
+  const [value, setValue] = useState("");
+  return (
+    <footer>
+      <Section className="container-footer">
+        <div className="footer-wrapper">
+          <div className="footer-children about-us">
+            <img src={imageSrc.logo} alt="" width={175} height={37} />
+            <p>
+              We are many variations of passages available the majority have
+              suffered alteration in some form by injected humour words
+              believable.
+            </p>
+          </div>
+          <div className="footer-children footer-menu">
+            <h3>MoPlay</h3>
+            <ul>
+              <li>
+                <Link to="/">About Us</Link>
+              </li>
+              <li>
+                <Link to="/">Testimonials</Link>
+              </li>
+              <li>
+                <Link to="/">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/">Terms Of Service</Link>
+              </li>
+              <li>
+                <Link to="/">Privacy policy</Link>
+              </li>
+              <li>
+                <Link to="/">Update News</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-children footer-menu">
+            <h3>Genre</h3>
+            <ul>
+              <li>
+                <Link to="/">Action</Link>
+              </li>
+              <li>
+                <Link to="/">Biography</Link>
+              </li>
+              <li>
+                <Link to="/">Documentary</Link>
+              </li>
+              <li>
+                <Link to="/">Adventure</Link>
+              </li>
+              <li>
+                <Link to="/">Talk Show</Link>
+              </li>
+              <li>
+                <Link to="/">Psychological</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-children footer-menu">
+            <h3>Support</h3>
+            <ul>
+              <li>
+                <Link to="/">Help Center</Link>
+              </li>
+              <li>
+                <Link to="/">FAQ's</Link>
+              </li>
+              <li>
+                <Link to="/">My Account</Link>
+              </li>
+              <li>
+                <Link to="/">Request Movie</Link>
+              </li>
+              <li>
+                <Link to="/">Support</Link>
+              </li>
+              <li>
+                <Link to="/">Media Center</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-children about-us">
+            <h3>Newsletter</h3>
+            <p>Subscribe Our Newsletter To Get Latest Update And News</p>
+            <form action="">
+              <input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                type="text"
+                placeholder="Your Email"
+              />
+              <button>Subscribe Now</button>
+            </form>
+          </div>
+        </div>
+      </Section>
+    </footer>
+  );
 }
 
 export default Footer;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Movies from "../components/Movies/Movies";
 import Header from "../components/Header/Header";
 import SlideShow from "../components/Banner/SlideShow";
 import Title from "../components/Title/Title";
+import Footer from "../components/Footer/Footer";
 
 function Home() {
   return (
@@ -15,6 +16,7 @@ function Home() {
         typeApi="top_rated"
         page={1}
       />
+      <Footer />
     </Title>
   );
 }
