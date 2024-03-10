a1cc13a17e7ccc5d84d391e0c1cb12665c95e58c
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-10 14:57:15 +0700

    Add SignIn Page

--- client/src/components/Banner/SlideShow.tsx
+++ client/src/components/Banner/SlideShow.tsx
@@ -55,7 +55,7 @@ function SlideShow() {
         prevEl: ".swiper-button-prev",
       }}
       loop
-      // autoplay={{ delay: 3000, disableOnInteraction: false }}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
       className="slideshow"
     >
       {topRateMovieLength.map((item) => (
@@ -90,71 +90,6 @@ function SlideShow() {
           </div>
         </SwiperSlide>
       ))}
-      {/* <SwiperSlide className="slideshow-content">
-        <div className="slideshow-img">
-          <img src={imageSrc.banner} alt="" />
-        </div>
-        <div className="slideshow-text">
-          <h1 className="slideshow-text-title">
-            Ant Man and the Wasp Quantaumania
-          </h1>
-          <div className="slideshow-text-other">
-            <p className="slideshow-text-top-rate">
-              <StarBorderRoundedIcon />
-              <span>9.2</span>
-            </p>
-            <p className="slideshow-text-year">2023</p>
-            <p className="slideshow-text-time">3h 15min</p>
-          </div>
-          <p className="slideshow-text-genre">Action</p>
-          <p className="slideshow-text-description">
-            There are many variations of passages orem psum available but the
-            majority have suffered alteration in some repeat predefined chunks
-            form injected humour.
-          </p>
-          <div className="slideshow-watch-detail">
-            <Link to="/" className="slideshow-play-now">
-              <PlayArrowRoundedIcon /> <span>Watch Now</span>
-            </Link>
-            <Link to="/" className="slideshow-detail">
-              <span>Detail</span>
-            </Link>
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide className="slideshow-content">
-        <div className="slideshow-img">
-          <img src={imageSrc.banner} alt="" />
-        </div>
-        <div className="slideshow-text">
-          <h1 className="slideshow-text-title">
-            Ant Man and the Wasp Quantaumania
-          </h1>
-          <div className="slideshow-text-other">
-            <p className="slideshow-text-top-rate">
-              <StarBorderRoundedIcon />
-              <span>9.2</span>
-            </p>
-            <p className="slideshow-text-year">2023</p>
-            <p className="slideshow-text-time">3h 15min</p>
-          </div>
-          <p className="slideshow-text-genre">Action</p>
-
-          <p className="slideshow-text-description">
-            There are many variations of passages orem psum available but the
-            majority have suffered alteration in some repeat predefined chunks
-            form injected humour.
-          </p>
-          <div className="slideshow-watch-detail">
-            <Link to="/" className="slideshow-play-now">
-              <PlayArrowRoundedIcon /> <span>Watch Now</span>
-            </Link>
-            <Link to="/" className="slideshow-detail">
-              <span>Detail</span>
-            </Link>
-          </div>
-        </div>
-      </SwiperSlide> */}
       <div className="swiper-button">
         <div className="swiper-button-next">
           <ArrowForwardIosRoundedIcon />
